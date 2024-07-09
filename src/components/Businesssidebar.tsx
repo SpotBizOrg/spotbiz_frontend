552fac52f69baec9cee269b1f091f2ec36543b9b
Nirasha Nelki <nirashanelki@gmail.com>
2024-07-09 09:20:43 +0530

    some changes

--- src/components/Businesssidebar.tsx
+++ src/components/Businesssidebar.tsx
@@ -63,7 +63,7 @@ function Businesssidebar({selectedTile}:BusinesssidebarProps){
               <a
                 href={item.href}
                 className={classNames(
-                  selectedTile == item.name ? 'bg-gray-900 text-gray-100' : 'text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-100',
+                  selectedTile == item.name ? 'bg-gray-900 text-gray-100' : 'text-gray-900 hover:bg-gray-500 hover:text-gray-100',
                   'group flex items-center px-3 py-2 text-sm font-medium rounded-lg'
                 )}
               >
