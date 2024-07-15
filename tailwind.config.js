98bedd62ef0cc0d640fc2ff00c3579e41cc73af7
Yuhanga Induwara <121022094+YuhangaInduwara@users.noreply.github.com>
2024-07-15 15:10:16 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -66,48 +66,9 @@ export default {
       backgroundColor: ['hover', 'focus'],
       textColor: ['hover', 'focus'],
       ringWidth: ['focus'],
-
-    fontFamily: {
-      body: ['"Popins"', 'sans-serif'], // "Poppins", sans-serif
     },
-    colors: {
-      blue: '#bfdbfe',
-      bluedark: "#0D3B66",
-      bluegray: '#ebf3fa',
-      bgyellow: '#FAF0CA',
-      black: '#000000',
-      white: '#FFFFFF',
-      gray: {
-        50: '#f9fafb',
-        100: '#f3f4f6',
-        200: '#e5e7eb',
-        300: '#d1d5db',
-        400: '#9ca3af',
-        500: '#6b7280',
-        600: '#4b5563',
-        700: '#374151',
-        800: '#1f2937',
-        900: '#111827',
-      },
-    },
-    fontSize: {
-      heading: '70px',
-      subheading: '40px',
-      subsubheading: '30px',
-      bodyfont: '12px',
-      bodysmall: '14px',
-      bodymedium: '16px',
-      bodylarge: '20px',
-    },
-    boxShadow: {
-      'left': '-4px 0 10px -6px rgba(0, 0, 0, 0.5)', // Shadow on the left side
-      'right': '4px 0 10px -6px rgba(0, 0, 0, 0.5)', // Shadow on the right side
-      'top': '0 -4px 10px -6px rgba(0, 0, 0, 0.5)', // Shadow on the top side
-      'bottom': '0 4px 10px -6px rgba(0, 0, 0, 0.5)', // Shadow on the bottom side
-    },
-   
   },
   plugins: [
     require('flowbite/plugin')
-],
-}
+  ],
+};
