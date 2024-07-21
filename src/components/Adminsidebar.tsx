fe691887889a6d22cf156c8be3efe9a924543275
Nirasha Nelki <nirashanelki@gmail.com>
2024-07-21 10:18:52 +0530

    updating sidebar icons

--- src/components/Adminsidebar.tsx
+++ src/components/Adminsidebar.tsx
@@ -1,21 +1,124 @@
 import React from 'react';
-import { FaHome, FaUser, FaClipboardList, FaCog } from 'react-icons/fa';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import StyleIcon from '@mui/icons-material/Style';
+import Person3Icon from '@mui/icons-material/Person3';
+import StoreIcon from '@mui/icons-material/Store';
+import FlagCircleIcon from '@mui/icons-material/FlagCircle';
+import ExtensionIcon from '@mui/icons-material/Extension';
+import LocalPoliceIcon from '@mui/icons-material/LocalPolice';
+import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
+import WidgetsIcon from '@mui/icons-material/Widgets';
 
 interface AdminsidebarProps {
   selectedTile: string;
 }
 
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true, ico: <FaHome />, label: <span className="ms-3">Dashboard</span> },
-  { name: 'Business Registration', href: '#', current: false, ico: <FaClipboardList />, label: <span className="ms-3">Business Registration</span> },
-  { name: 'Categories & Tags', href: '#', current: false, ico: <FaClipboardList />, label: <span className="ms-3">Categories & Tags</span> },
-  { name: 'Customer List', href: '#', current: false, ico: <FaUser />, label: <span className="ms-3">Customer List</span> },
-  { name: 'Business List', href: '#', current: false, ico: <FaClipboardList />, label: <span className="ms-3">Business List</span> },
-  { name: 'Appeals & Reports', href: '#', current: false, ico: <FaClipboardList />, label: <span className="ms-3">Appeals & Reports</span> },
-  { name: 'Games', href: '#', current: false, ico: <FaClipboardList />, label: <span className="ms-3">Games</span> },
-  { name: 'Badges', href: '#', current: false, ico: <FaClipboardList />, label: <span className="ms-3">Badges</span> },
-  { name: 'Coupons', href: '#', current: false, ico: <FaClipboardList />, label: <span className="ms-3">Coupons</span> },
-  { name: 'Subscription Packages', href: '#', current: false, ico: <FaClipboardList />, label: <span className="ms-3">Subscription Packages</span> },
+
+interface NavigationItem {
+  name: string;
+  href: string;
+  current: boolean;
+  ico: React.ReactNode;
+  label:React.ReactNode;
+}
+
+// const navigation: NavigationItem[] = [
+//   { name: 'Dashboard', href: '#', current: true, ico: <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+//     <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+//     <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+//   </svg>, 
+//   label:<span className="ms-3">Dashboard</span>
+// },
+//   { name: 'Business Registration', href: '#', current: false, ico: <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+//     <path fill-rule="evenodd" d="M10 2a3 3 0 0 0-3 3v1H5a3 3 0 0 0-3 3v2.382l1.447.723.005.003.027.013.12.056c.108.05.272.123.486.212.429.177 1.056.416 1.834.655C7.481 13.524 9.63 14 12 14c2.372 0 4.52-.475 6.08-.956.78-.24 1.406-.478 1.835-.655a14.028 14.028 0 0 0 .606-.268l.027-.013.005-.002L22 11.381V9a3 3 0 0 0-3-3h-2V5a3 3 0 0 0-3-3h-4Zm5 4V5a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v1h6Zm6.447 7.894.553-.276V19a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-5.382l.553.276.002.002.004.002.013.006.041.02.151.07c.13.06.318.144.557.242.478.198 1.163.46 2.01.72C7.019 15.476 9.37 16 12 16c2.628 0 4.98-.525 6.67-1.044a22.95 22.95 0 0 0 2.01-.72 15.994 15.994 0 0 0 .707-.312l.041-.02.013-.006.004-.002.001-.001-.431-.866.432.865ZM12 10a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H12Z" clip-rule="evenodd"/>
+//     </svg>,
+//     label:<span className="ms-3">Business Registration</span>
+// },
+//   { name: 'Categories & tags', href: '#', current: false, ico: <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+//     <path d="M18.045 3.007 12.31 3a1.965 1.965 0 0 0-1.4.585l-7.33 7.394a2 2 0 0 0 0 2.805l6.573 6.631a1.957 1.957 0 0 0 1.4.585 1.965 1.965 0 0 0 1.4-.585l7.409-7.477A2 2 0 0 0 21 11.479v-5.5a2.972 2.972 0 0 0-2.955-2.972Zm-2.452 6.438a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>
+//     </svg>,
+//     label:<span className="ms-3">Categories & Tags</span>
+
+// },
+//   { name: 'Customer List', href: '#', current: false, ico:<svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+//     <path fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H6Zm7.25-2.095c.478-.86.75-1.85.75-2.905a5.973 5.973 0 0 0-.75-2.906 4 4 0 1 1 0 5.811ZM15.466 20c.34-.588.535-1.271.535-2v-1a5.978 5.978 0 0 0-1.528-4H18a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2h-4.535Z" clip-rule="evenodd"/>
+//     </svg>,
+//     label:<span className="ms-3">Customer List</span>
+// },
+//   { name: 'Business List', href: '#', current: false, ico:<svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+//     <path fill-rule="evenodd" d="M4 4a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2v14a1 1 0 1 1 0 2H5a1 1 0 1 1 0-2V5a1 1 0 0 1-1-1Zm5 2a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H9Zm5 0a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-1Zm-5 4a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1H9Zm5 0a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1h-1Zm-3 4a2 2 0 0 0-2 2v3h2v-3h2v3h2v-3a2 2 0 0 0-2-2h-2Z" clip-rule="evenodd"/>
+//     </svg>,
+//     label:<span className="ms-3">Business List</span>
+
+// },
+//   { name: 'Appeals & Reports', href: '#', current: false, ico:<svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+//     <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z" clip-rule="evenodd"/>
+//     </svg>,
+//     label:<span className="ms-3">Appeals & Reports</span>
+// },
+//   { name: 'Games', href: '#', current: false, ico:<svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+//     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z" />
+//   </svg>,
+//   label:<span className="ms-3">Games</span>
+
+// },
+//   { name: 'Badges', href: '#', current: false, ico:<svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+//     <path fill-rule="evenodd" d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z" clip-rule="evenodd"/>
+//     </svg>,
+//     label:<span className="ms-3">Badges</span>
+
+// },
+//   { name: 'Cupons', href: '#', current: false, ico: <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+//     <path fill-rule="evenodd" d="M5.617 2.076a1 1 0 0 1 1.09.217L8 3.586l1.293-1.293a1 1 0 0 1 1.414 0L12 3.586l1.293-1.293a1 1 0 0 1 1.414 0L16 3.586l1.293-1.293A1 1 0 0 1 19 3v18a1 1 0 0 1-1.707.707L16 20.414l-1.293 1.293a1 1 0 0 1-1.414 0L12 20.414l-1.293 1.293a1 1 0 0 1-1.414 0L8 20.414l-1.293 1.293A1 1 0 0 1 5 21V3a1 1 0 0 1 .617-.924ZM9 7a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2H9Zm0 4a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2H9Zm0 4a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2H9Z" clip-rule="evenodd"/>
+//     </svg>,
+//     label:<span className="ms-3">Cupons</span>
+
+// },
+//   { name: 'Subscription Packages', href: '#', current: false, ico:<svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+//     <path d="M12.378 1.602a.75.75 0 0 0-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03ZM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 0 0 .372-.648V7.93ZM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 0 0 .372.648l8.628 5.033Z" />
+//     </svg>,
+//     label:<span className="ms-3">Subscription Packages</span>
+
+// },
+// ];
+const navigation: NavigationItem[] = [
+  { name: 'Dashboard', href: '#', current: true, ico: <DashboardIcon />, 
+  label:<span className="ms-3">Dashboard</span>
+},
+  { name: 'Business Registration', href: '#', current: false, ico: <BusinessCenterIcon />,
+    label:<span className="ms-3">Business Registration</span>
+},
+  { name: 'Categories & tags', href: '#', current: false, ico: <StyleIcon />,
+    label:<span className="ms-3">Categories & Tags</span>
+
+},
+  { name: 'Customer List', href: '#', current: false, ico:<Person3Icon />,
+    label:<span className="ms-3">Customer List</span>
+},
+  { name: 'Business List', href: '#', current: false, ico:<StoreIcon />,
+    label:<span className="ms-3">Business List</span>
+
+},
+  { name: 'Appeals & Reports', href: '#', current: false, ico:<FlagCircleIcon />,
+    label:<span className="ms-3">Appeals & Reports</span>
+},
+  { name: 'Games', href: '#', current: false, ico:<ExtensionIcon />,
+  label:<span className="ms-3">Games</span>
+
+},
+  { name: 'Badges', href: '#', current: false, ico:<LocalPoliceIcon />,
+    label:<span className="ms-3">Badges</span>
+
+},
+  { name: 'Cupons', href: '#', current: false, ico: <RequestQuoteIcon />,
+    label:<span className="ms-3">Cupons</span>
+
+},
+  { name: 'Subscription Packages', href: '#', current: false, ico:<WidgetsIcon />,
+    label:<span className="ms-3">Subscription Packages</span>
+
+},
 ];
 
 function classNames(...classes: string[]): string {
