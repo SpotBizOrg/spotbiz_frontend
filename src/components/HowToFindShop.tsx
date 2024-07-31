6302710828fbd90be7dcbee037a00ccdd9b65788
Yuhanga Induwara <121022094+YuhangaInduwara@users.noreply.github.com>
2024-07-31 10:27:11 +0530

    Changed admin dashboard

--- src/components/HowToFindShop.tsx
+++ src/components/HowToFindShop.tsx
@@ -11,13 +11,13 @@ const HowToFindShop = () => {
   return (
     <div className="flex items-center bg-blue10 p-8 rounded-custom3 shadow-lg mx-4 md:mx-8 lg:mx-16">
       <div className="mr-8 text-left">
-        <h2 className="text-3xl font-bold mb-4">How to Find a Shop and their offers at SpotBizz?</h2>
+        <h2 className="text-3xl font-bold mb-4">How to Find a Shop and their offers at SpotBiz?</h2>
         <p className="text-lg mb-6">
-          Discover a variety of shops and their exclusive offers on SpotBizz. 
+          Discover a variety of shops and their exclusive offers on SpotBiz. 
           Here's how you can find them:
         </p>
         <ul className="list-disc list-inside text-lg space-y-2">
-          <li>Go to SpotBizz website or download the SpotBizz mobile app</li>
+          <li>Go to SpotBiz website or download the SpotBiz mobile app</li>
           <li>Search for shops by category or location</li>
           <li>Stay updated on their latest offers and discounts</li>
           <li>Engage with games and earn points</li>
