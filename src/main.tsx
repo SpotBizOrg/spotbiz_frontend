3bf116bbfbdc6bab24e5a459b7f520025f99aaaa
YuhangaInduwara <yuhanga2001@gmail.com>
2024-10-12 19:59:07 +0530

    Added notification

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,16 @@ import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './utils/AuthProvider';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
