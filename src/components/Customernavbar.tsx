266722612dbe97c4ecd38658b7f672ba7f3e5432
Nirasha Nelki <nirashanelki@gmail.com>
2024-12-02 09:43:37 +0530

    setting customer profile pic in navbar

--- src/components/Customernavbar.tsx
+++ src/components/Customernavbar.tsx
@@ -71,6 +71,8 @@ function Customernavbar() {
     }
   };
 
+  
+
     return(
     <nav className="fixed top-0 z-40 w-full bg-white border-b border-gray-200">
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
