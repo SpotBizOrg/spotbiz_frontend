1cb9452b4fe735dac6b0e1a7be6c797e8183278a
Nirasha Nelki <nirashanelki@gmail.com>
2024-12-03 12:49:59 +0530

    transaction report generation backend connected

--- src/pages/AdminTransactions.tsx
+++ src/pages/AdminTransactions.tsx
@@ -4,9 +4,37 @@ import Adminsidebar from "../components/Adminsidebar";
 import Container from "../components/Container";
 import SubscriptionTransactions from "../components/SubscriptionTransactions";
 import ReimbursementTransactions from "../components/ReimbursementTransactions";
+import { Button } from "flowbite-react";
+import axios from "axios";
+import { BACKEND_URL } from "../../config";
+import { HashLoader } from "react-spinners";
 
 const AdminTransactions = () => {
     const [activeTab, setActiveTab] = useState('subscription');
+    const [loading, setLoading] = useState(false);
+
+    const downloadReport = async () => {
+        setLoading(true);
+        try {
+            
+            const url = `${BACKEND_URL}/transaction-report`;
+
+            const response = await axios.get(url, {
+                responseType: 'blob', // Ensures the response is treated as a binary file
+            });
+    
+            // Create a link to download the PDF file
+            const blob = new Blob([response.data], { type: 'application/pdf' });
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = 'transaction_history_report.pdf'; 
+            link.click(); // Simulate a click to trigger the download
+        } catch (error) {
+            console.error('Error downloading the report:', error);
+        } finally{
+            setLoading(false);
+        }
+    };
 
     
     return (
@@ -18,8 +46,26 @@ const AdminTransactions = () => {
                 <div className="w-full mb-5 border-b boder-gray-900">
                     <h1 className="text-subsubheading text-bluedark">Transactions</h1>
                 </div>
+                
+                <div className="relative  overflow-x-auto overflow-y-auto sm:rounded-lg mb-2">
+                <div className="flex items-center justify-between w-full">
+
+                    <div className="flex items-center space-x-6"></div>
+                              {/* Add the download report button */}
+                <Button
+                        onClick={downloadReport}
+                        size="sm"
+                        color="dark"
+                        // className="mb-4 px-4 py-2 bg-blue-500 text-white rounded-md"
+                    >
+                        Download Detailed Report
+                    </Button>
+
+                    </div>
+                </div>
 
                 <div className="relative  overflow-x-auto overflow-y-auto sm:rounded-lg">
+
                     {/* the tabs */}
                 <div className="flex items-center space-x-6 mb-5 border-b border-gray-200">
                     <button
@@ -38,6 +84,8 @@ const AdminTransactions = () => {
                 </div>
                 </div>
 
+          
+
                 {activeTab === 'subscription' &&
                     <div className="relative overflow-x-auto overflow-y-auto sm:rounded-lg border border-gray-200">
                     <SubscriptionTransactions/>
@@ -49,6 +97,13 @@ const AdminTransactions = () => {
 
 
                 </div>
+                <div className="px-12 sm:ml-64 mt-20">
+                {loading && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                    <HashLoader color="#36d7b7" size={50} />
+                </div>
+                )}
+            </div>
                 </Container>
         </>
     );
